3d9da2fd820106fa511d099f3d7f63d61e93b6a9
dxzhang <dxzhang@ustc.edu>
2015-04-03 15:04:57 +0800

    add generation scripts

---
+++ generate_linux_dts.sh
@@ -0,0 +1,1 @@
+java -jar sopc2dts.jar --input=../DE2_70_SOPC.sopcinfo --output=terasic_de2_70_devboard.dts --type=dts --force-altr
---
+++ generate_uboot_header.sh
@@ -0,0 +1,1 @@
+java -jar sopc2dts.jar --input=../DE2_70_SOPC.sopcinfo --type=uboot --output=custom_fpga.h
